605270e06c8d70c557203496c07a4aa065b897ca
Iñigo Aldaraborda Hoyos <inigo.aldaraborda-hoyos@epita.fr>
2024-06-06 15:57:32 +0200

    new events + calendar colors

--- src/components/FloatingCalendar.jsx
+++ src/components/FloatingCalendar.jsx
@@ -9,13 +9,71 @@ import {VscClose} from "react-icons/vsc";
 const localizer = momentLocalizer(moment);
 
 const FloatingCalendar = ({onClose}) => {
+    // use only pastel colors
     const events = [
         {
-            title: 'Some Event',
-            start: new Date(),
-            end: new Date(moment().add(1, 'days')),
-            allDay: true
-        }
+            title: 'Présentation Maquette',
+            start: new Date(moment().toDate()),
+            end: new Date(moment().add(1, 'hour').toDate()),
+            allDay: false,
+            color: '#A5DD9B'
+        },
+        {
+            title: 'Business Call',
+            start: new Date(moment().subtract(2, 'hour').toDate()),
+            end: new Date(moment().subtract(1, 'hour').toDate()),
+            allDay: false,
+            color: '#F2C18D'
+        },
+        {
+            title: 'Rest',
+            start: new Date(moment().add(1, 'hour').toDate()),
+            end: new Date(moment().add(2, 'hour').toDate()),
+            allDay: false,
+            color: '#FFD3B5'
+        },
+        {
+            title: 'Meeting',
+            start: new Date(moment().add(3, 'hour').toDate()),
+            end: new Date(moment().add(4, 'hour').toDate()),
+            allDay: false,
+            color: '#A5DD9B'
+        },
+        {
+            title: 'Dentist',
+            start: new Date(moment().add(1, 'day').add(1, 'hour').toDate()),
+            end: new Date(moment().add(1, 'day').add(2, 'hour').toDate()),
+            allDay: false,
+            color: '#FFD3B5'
+        },
+        {
+            title: 'Day Off',
+            start: new Date(moment().add(1, 'day').add(2, 'hour').toDate()),
+            end: new Date(moment().add(1, 'day').add(3, 'hour').toDate()),
+            allDay: true,
+            color: '#BED7DC'
+        },
+        {
+            title: 'Meeting',
+            start: new Date(moment().add(2, 'day').toDate()),
+            end: new Date(moment().add(2, 'day').add(1, 'hour').toDate()),
+            allDay: false,
+            color: '#A5DD9B'
+        },
+        {
+            title: 'Meeting',
+            start: new Date(moment().add(3, 'day').toDate()),
+            end: new Date(moment().add(3, 'day').add(1, 'hour').toDate()),
+            allDay: false,
+            color: '#A5DD9B'
+        },
+        {
+            title: 'Meeting',
+            start: new Date(moment().add(4, 'day').toDate()),
+            end: new Date(moment().add(4, 'day').add(1, 'hour').toDate()),
+            allDay: false,
+            color: '#A5DD9B'
+        },
     ];
     return (
         <div className="floating-window">
@@ -33,6 +91,14 @@ const FloatingCalendar = ({onClose}) => {
                         style={{height: "90vh", width: "90vw"}}
                         defaultView="day"
                         selectable
+                        eventPropGetter={(event) => {
+                            return {
+                                style: {
+                                    backgroundColor: event.color
+                                }
+                            }
+                        }}
+                        onSelectEvent={event => console.log(event)}
                     />
                 </div>
             </div>
