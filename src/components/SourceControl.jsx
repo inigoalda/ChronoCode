ca66f924e8e066f41e8454ef49df61ead6b95734
Jimmy Fabre <jimmy.fabre@epita.fr>
2024-06-27 23:40:13 +0200

    test commit with front

--- src/components/SourceControl.jsx
+++ src/components/SourceControl.jsx
@@ -4,6 +4,8 @@ import './SourceControl.css'
 const SourceControl = () => {
     const [files, setFiles] = useState([]);
     const [pull, setPull] = useState("Pull");
+    const [push, setPush] = useState("Push");
+    const [filesCheck, setFilesCheck] = useState([]);
 
     const handlePull = async () => {
         const execData = {
@@ -13,8 +15,9 @@ const SourceControl = () => {
         };
 
         try {
-            setPull("Loading...")
-            setFiles([])
+            setPull("Loading...");
+            setFiles([]);
+            setFilesCheck([]);
             const response = await fetch('http://localhost:8080/api/execFeature', {
                 method: 'POST',
                 headers: {
@@ -24,7 +27,6 @@ const SourceControl = () => {
             });
             if (response.ok) {
                 const data = await response.json();
-                setFiles(data.added);
                 setFiles(prevFiles => {
                     const merge = [...prevFiles, ...data.modified, ...data.untracked];
                     return merge;
@@ -40,8 +42,81 @@ const SourceControl = () => {
         setPull("Pull")
     };
 
-    const handlePush = () => {
-        // TODO
+    const handlePush = async () => {
+
+        var execData = {
+            feature: "ADD",
+            params: filesCheck,
+            project: "useless"
+        };
+
+        try {
+            setPush("Loading...");
+            setFiles([]);
+            const response = await fetch('http://localhost:8080/api/execFeature', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(execData),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setFiles(prevFiles => {
+                    const merge = [...prevFiles, ...data.modified, ...data.untracked];
+                    return merge;
+                });
+            } else {
+                const errorData = await response.json();
+                setFiles(["Error"]);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setFiles(["Error"]);
+        }
+
+        execData = {
+            feature: "COMMIT",
+            params: ["test commit with front"],
+            project: "useless"
+        };
+
+        try {
+            setFiles([]);
+            const response = await fetch('http://localhost:8080/api/execFeature', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(execData),
+            });
+            if (response.ok) {
+            } else {
+                const errorData = await response.json();
+                setFiles(["Error"]);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setFiles(["Error"]);
+        }
+
+        setPush("Push")
+        setFilesCheck([]);
+    };
+
+    const handleCheck = (e) => {
+        const file = e.target.attributes.path.nodeValue;
+        if (filesCheck.includes(file)) {
+            setFilesCheck(filesCheck.filter(e => e != file));
+        }
+        else {
+            setFilesCheck(
+                prev => {
+                    const merge = [...prev, file];
+                    return merge;
+                }
+            );
+        }
     };
 
     return (
@@ -58,13 +133,13 @@ const SourceControl = () => {
             <div className="divlistGit">
                 <ul className="Ullist">
                     {files.map((file, index) => (
-                        <li className="filelist" key={index}><input type="checkbox"></input><p className="file">{file.split('/').pop()}</p><p className="path">{file}</p><p></p></li>
+                        <li className="filelist" key={index}><input path={file} type="checkbox" onClick={handleCheck}></input><p className="file">{file.split('/').pop()}</p><p className="path">{file}</p><p></p></li>
                     ))}
                 </ul>
             </div>
             <div className={"source-control"}>
                 <button className="button" onClick={handlePull}>{pull}</button>
-                <button className="button" onClick={handlePush}>Push</button>
+                <button className="button" onClick={handlePush}>{push}</button>
             </div>
         </div >
     );
