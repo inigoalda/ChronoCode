8fd4897c561dfd228d6d307cfc9c2dfc3a082635
Fabien <fabien.burguion@epita.fr>
2024-07-01 00:04:53 +0200

    Styling: display No results for SEARCH + make new file disappear when clicked + search can now find multiple occurences on one line

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-
 import './SearchBar.css';
 import Result from './Result';
 
@@ -8,14 +7,42 @@ const SearchBar = (props) => {
     const textareaRef = useRef(null);
     const [loading, setLoading] = useState(false);
     const [results, setResults] = useState([]);
+    const [searchPerformed, setSearchPerformed] = useState(false);
+
+
+    function get_language(s) {
+        const extension = s.split('.').pop().toLowerCase();
+        switch (extension) {
+            case 'java':
+                return 'java';
+            case 'py':
+                return 'python';
+            case 'js':
+                return 'javascript';
+            case 'c':
+                return 'c';
+            case 'cpp':
+            case 'cxx':
+            case 'cc':
+            case 'c++':
+                return 'cpp';
+            case 'txt':
+                return 'text';
+            default:
+                return 'plaintext'
+        }
+    }
 
     const handleSearch = async () => {
-        if (search === "")
-        {
+        if (search.replace(/\s/g, '').length === 0) {
+            setResults([]);
+            setSearchPerformed(true);
             return;
         }
-        let results = [];
+        let searchResults = [];
         setLoading(true);
+        setSearchPerformed(true);
+
         if (props.tabs) {
             props.tabs.forEach(tab => {
                 const key = tab.key;
@@ -31,17 +58,15 @@ const SearchBar = (props) => {
                         if (i === -1) {
                             break;
                         }
-                        results.push(
-                            {
-                                key: key,
-                                title: title,
-                                content: line,
-                                path: path,
-                                line: index + 1,
-                                position: i,
-                                language: language
-                            }
-                        );
+                        searchResults.push({
+                            key: key,
+                            title: title,
+                            content: line,
+                            path: path,
+                            line: index + 1,
+                            position: i,
+                            language: language
+                        });
                         i += search.length;
                     }
                 });
@@ -55,42 +80,41 @@ const SearchBar = (props) => {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ feature: "SEARCH", 
-                        params : [search], 
-                        project: "project" }), // Fix project maybe
+                    body: JSON.stringify({ feature: "SEARCH", params: [search], project: "project" }),
                 });
                 if (!response.ok) {
-                    throw new Error('File not found');
+                    console.log("Bad response");
+                    setResults([]);
+                    setLoading(false);
+                    return;
                 }
                 const data = await response.json();
-                console.log(data);
                 for (const item of data.l) {
-                    console.log("Item: " + item);
-                    console.log(typeof item.line, item.line);
-                    if (!results.some(result => result.path === item.path))
-                    {
-                        results.push({
+                    if (!searchResults.some(result => result.path === item.path)) {
+                        searchResults.push({
                             key: item.key,
                             title: item.title,
                             content: item.content,
                             path: item.path,
                             line: item.line,
                             position: item.position,
-                            language: ""
-                        })
+                            language: get_language(item.title)
+                        });
                     }
                 }
             } catch (error) {
                 console.log(error.message);
             }
         }
-        setResults(results);
+
+        setResults(searchResults);
         setLoading(false);
     };
 
     const handleChange = (event) => {
         setSearch(event.target.value);
         setResults([]);
+        setSearchPerformed(false);
     };
 
     useEffect(() => {
@@ -101,15 +125,13 @@ const SearchBar = (props) => {
 
     return (
         <div>
-            <p style={
-                {
-                    color: "white",
-                    margin: "0",
-                    padding: "10px",
-                    paddingLeft: "20px",
-                    fontSize: "11px"
-                }
-            }>
+            <p style={{
+                color: "white",
+                margin: "0",
+                padding: "10px",
+                paddingLeft: "20px",
+                fontSize: "11px"
+            }}>
                 SEARCH
             </p>
             <div className='search-bar-wrapper'>
@@ -120,11 +142,11 @@ const SearchBar = (props) => {
                     value={search}
                     onChange={handleChange}
                 />
-                {!props.data && <p style={{color: "white", marginLeft: 20, marginRight: 20, marginBottom: 20}}>You have not opened or specified a folder. Only open files are currently searched - <a href="#" onClick={props.openFolder} style={{color: "#017BFD", textDecoration: "none"}}>Open Folder</a></p>}
+                {!props.data && <p style={{ color: "white", marginLeft: 20, marginRight: 20, marginBottom: 20 }}>You have not opened or specified a folder. Only open files are currently searched - <a href="#" onClick={props.openFolder} style={{ color: "#017BFD", textDecoration: "none" }}>Open Folder</a></p>}
                 <button className="search-button" onClick={handleSearch}>Search</button>
                 {loading && <p>Loading...</p>}
                 <div className="search-results">
-                    {results.map((result, index) => <Result key={index} result={result} query={search} onClick={() => props.onResultClick(result)}/>)}
+                    {searchPerformed && results.length === 0 && !loading ? <div className="no-results">No results</div> : results.map((result, index) => <Result key={index} result={result} query={search} onClick={() => props.onResultClick(result)} />)}
                 </div>
             </div>
         </div>
